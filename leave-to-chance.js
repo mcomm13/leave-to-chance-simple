a067ceb4fcd6c14b95fddedf166603cfa3d9059d
Mike Commendatore <michael.commendatore@outlook.com>
2021-01-15 15:51:29 -0500

    Added styles and initialized Firebase

--- leave-to-chance.js
+++ leave-to-chance.js
@@ -3,6 +3,7 @@ const resultSpan = document.getElementById('random-pick');
 
 const makeRandomPick = () => {
   resultSection.hidden = true;
+  // resultSection.classList.remove('animate__animated animate__rubberBand');
   let randomPick = 0;
   let numberOfOptions = document.getElementById('input-options').value || 2;
 
@@ -10,6 +11,7 @@ const makeRandomPick = () => {
     numberOfOptions = parseInt(numberOfOptions);
     randomPick = Math.floor(Math.random() * numberOfOptions) + 1;
     resultSpan.textContent = randomPick.toString();
+    //resultSection.classList.add('animate__animatedanimate__rubberBand');
     resultSection.hidden = false;
   }, 300);
 };
